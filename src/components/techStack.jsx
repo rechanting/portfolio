4ffbd43eeb318e4703a1bf61d8c416ee0f51d80d
Hamza Elashry <98109755+arkseek@users.noreply.github.com>
2024-04-16 12:15:16 -0400

    Update techStack.jsx

--- src/components/techStack.jsx
+++ src/components/techStack.jsx
@@ -7,11 +7,9 @@ export default function Stack() {
 
     return (
         <div className="flex">
-            <img src="/images/nodejs.png" alt="nodejs" style={imageStyle}  className="object-scale-down"/>
             <img src="/images/astro.png" alt="astro" style={imageStyle} className="object-scale-down"/>
             <img src="/images/tailwind.png" alt="tailwind" style={imageStyle} className="object-scale-down"/>
             <img src="/images/git.png" alt="git" style={imageStyle} className="object-scale-down"/>
-            <img src="/images/github.png" alt="github" style={imageStyle} className="object-scale-down"/>
             <img src="/images/vercel.png" alt="vercel" style={imageStyle} className="object-scale-down"/>
         </div>
     );
