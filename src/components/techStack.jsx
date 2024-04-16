14f68c8324ea5fd607f656b99afc2faf93bd2712
Hamza Elashry <98109755+arkseek@users.noreply.github.com>
2024-04-16 12:13:51 -0400

    Update techStack.jsx
    added object-scale-down to make it responsive

--- src/components/techStack.jsx
+++ src/components/techStack.jsx
@@ -2,18 +2,17 @@ export default function Stack() {
     const imageStyle = {
         width:'75px',
         height: '75px',
-        objectFit: 'cover',
         marginRight: '14px'
     };
 
     return (
         <div className="flex">
-            <img src="/images/nodejs.png" alt="nodejs" style={imageStyle} />
-            <img src="/images/astro.png" alt="astro" style={imageStyle} />
-            <img src="/images/tailwind.png" alt="tailwind" style={imageStyle} />
-            <img src="/images/git.png" alt="git" style={imageStyle} />
-            <img src="/images/github.png" alt="github" style={imageStyle} />
-            <img src="/images/vercel.png" alt="vercel" style={imageStyle} />
+            <img src="/images/nodejs.png" alt="nodejs" style={imageStyle}  className="object-scale-down"/>
+            <img src="/images/astro.png" alt="astro" style={imageStyle} className="object-scale-down"/>
+            <img src="/images/tailwind.png" alt="tailwind" style={imageStyle} className="object-scale-down"/>
+            <img src="/images/git.png" alt="git" style={imageStyle} className="object-scale-down"/>
+            <img src="/images/github.png" alt="github" style={imageStyle} className="object-scale-down"/>
+            <img src="/images/vercel.png" alt="vercel" style={imageStyle} className="object-scale-down"/>
         </div>
     );
 }
